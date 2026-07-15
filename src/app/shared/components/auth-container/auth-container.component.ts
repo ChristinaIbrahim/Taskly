1b58bfb65c2a1532ce2e39e299cc90e7f4865fb0
Christina <christina.i.aziz7@gmail.com>
2026-07-15 12:44:28 +0300

    feat(styles): add global design tokens and utitlty classes

--- src/app/shared/components/auth-container/auth-container.component.ts
+++ src/app/shared/components/auth-container/auth-container.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input , ChangeDetectionStrategy } from '@angular/core';
+import { NavLogoComponent } from '../nav-logo/nav-logo.component';
+import { NgClass } from '@angular/common';
+
 
 @Component({
   selector: 'app-auth-container',
   standalone: true,
-  imports: [],
+  imports: [NgClass,NavLogoComponent],
   templateUrl: './auth-container.component.html',
   styleUrl: './auth-container.component.css'
 })
@@ -12,4 +15,15 @@ export class AuthContainerComponent {
   @Input() title: string = '';
   @Input() description: string = '';
 
+  @Input() mobileDescription = '';
+
+  @Input() backgroundColor = 'bg-background md:bg-white';
+  @Input() contentClass = 'p-6 md:p-12';
+  @Input() titleClass = 'text-start md:text-center';
+  @Input() descriptionClass = 'md:text-center text-start';
+  @Input() mobileDescriptionClass = 'text-start';
+  @Input() hightClass = 'min-h-[90vh] md:min-h-auto pb-12';
+  @Input() headerOutside = false; 
 }
+
+
--- src/app/shared/components/nav-logo/nav-logo.component.ts
+++ src/app/shared/components/nav-logo/nav-logo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AppIconsDirective } from '../../directives/app-icons.directive';
 
 @Component({
   selector: 'app-nav-logo',
   standalone: true,
-  imports: [],
+  imports: [AppIconsDirective],
   templateUrl: './nav-logo.component.html',
   styleUrl: './nav-logo.component.css'
 })
