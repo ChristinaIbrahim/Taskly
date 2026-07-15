a138e3be12a576183008c3bac7941520e6d1e18b
Christina <christina.i.aziz7@gmail.com>
2026-07-16 00:22:16 +0300

    setup supabase endpoint in environmemt variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://ydfdgnmkmpobecxnkjbu.supabase.co/',
+  supabase_api_key: 'sb_publishable_R2NQQmStwtA3XQpLlzihlw_BpSCLDIW',
+};
