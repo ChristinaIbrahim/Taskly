fc8e24c0419273fc07cfc7373668e9ba1cd9d797
Christina <christina.i.aziz7@gmail.com>
2026-07-16 18:24:14 +0300

    chore(router): configure core layout and auth routing roles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,38 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+  {
+    path: '', 
+    redirectTo: 'sign-up', 
+    pathMatch: 'full'
+  },
   {
     path: 'sign-up',
     loadComponent: () => import('./features/auth/signup/signup.component').then(m => m.SignupComponent)
   },
   {
     path: 'login',
-    loadComponent: () => import('./features/auth/login/login.component').then(m =>m.LoginComponent),
+    loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent),
   },
-  // {
-  //   path: 'project',
-  //   loadComponent: () => import('./pages/project/project.component').then(m => m.ProjectComponent),
-  //   children:[
-  //     {
-  //       path:'add-project',
-  //       loadComponent: () => import('./pages/project/add-project/add-project.component').then(m=>m.AddProjectComponent)
-  //     }
-  //   ]
-
-  // },
-  //  {
-  //   path: 'forget-password',
-  //   loadComponent: () => import('./pages/forget-password/forget-password.component').then(m => m.ForgetPasswordComponent)
-
-  // },
 
   {
-    path: '', redirectTo: 'sign-up', pathMatch: 'full'
+    path: '',
+    loadComponent: () => import('./core/layout/layout.component').then(m => m.LayoutComponent),
+    children: [
+      {
+        path: 'projects',
+        loadComponent: () => import('./features/projects/projects.component').then(m => m.ProjectsComponent)
+      },
+      { 
+        path: '', 
+        redirectTo: 'projects', 
+        pathMatch: 'full' 
+      }
+    ]
   },
 
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
