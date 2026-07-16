cb03b5578677c27bd716e612a15d08eda93ec397
Christina <christina.i.aziz7@gmail.com>
2026-07-16 11:37:49 +0300

    refactor: update login component to clean version

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+import { AuthContainerComponent } from '../../../shared/components/auth-container/auth-container.component';
+import { FormFieldComponent } from '../../../shared/components/form-field/form-field.component';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,         
+    ReactiveFormsModule,   
+    RouterModule,         
+    AuthContainerComponent, 
+    FormFieldComponent,  
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -14,7 +24,6 @@ export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   errorMessage: string | null = null;
   isLoading = false;
-  showPassword = false;
 
   constructor(
     private fb: FormBuilder,
@@ -34,11 +43,8 @@ export class LoginComponent implements OnInit {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]], 
       password: ['', [Validators.required]],
+      rememberMe: [false] 
     });
-  }
-
-  togglePasswordVisibility() {
-    this.showPassword = !this.showPassword;
   }
 
   onSubmit() {
@@ -67,5 +73,4 @@ export class LoginComponent implements OnInit {
       }
     });
   }
-
 }
