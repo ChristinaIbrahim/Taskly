0a1c73918c9fd0bff6e96176403f6cab04702746
Christina <christina.i.aziz7@gmail.com>
2026-07-18 17:50:29 +0300

    feat: implement add empty-projects component layout and navigation

--- src/app/features/project/components/empty-projects/empty-projects.component.ts
+++ src/app/features/project/components/empty-projects/empty-projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-empty-projects',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './empty-projects.component.html',
   styleUrl: './empty-projects.component.css'
 })
