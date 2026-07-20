5d9043bea7ac5da8ec6c64c98e2cf8be6ea903bc
Christina <christina.i.aziz7@gmail.com>
2026-07-20 10:28:42 +0300

    feat(project): implement edit button and fix date binding on project card
    - Added stylized edit SVG icon with routing to project edit page. Imported RouterLink and CommonModule to resolve NG8002 template compiler errors

--- src/app/features/project/components/card-project/card-project.component.ts
+++ src/app/features/project/components/card-project/card-project.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card-project',
   standalone: true,
-  imports: [CommonModule, DatePipe],
+  imports: [CommonModule, DatePipe , RouterLink],
   templateUrl: './card-project.component.html',
   styleUrl: './card-project.component.css',
 })
