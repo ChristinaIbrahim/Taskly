1767bb773bc4dff89346b7d5248c38d50df1f322
Christina <christina.i.aziz7@gmail.com>
2026-07-25 01:05:41 +0300

    style(tasks): update breadcrumb routerlink, add icons and style header controls

--- src/app/features/tasks/tasks.component.ts
+++ src/app/features/tasks/tasks.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { BoardViewTaskComponent } from './components/board-view-task/board-view-task.component';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [BoardViewTaskComponent, CommonModule],
+  imports: [BoardViewTaskComponent, CommonModule , RouterLink],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
