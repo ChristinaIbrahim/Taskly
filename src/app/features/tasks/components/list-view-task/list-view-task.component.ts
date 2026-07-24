899277b917fe90eb4ba371a0b03fbb3ab9f46360
Christina <christina.i.aziz7@gmail.com>
2026-07-25 02:26:51 +0300

    feat(tasks): create list-view-task component structure

---
+++ src/app/features/tasks/components/list-view-task/list-view-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-view-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-view-task.component.html',
+  styleUrl: './list-view-task.component.css'
+})
+export class ListViewTaskComponent {
+
+}
