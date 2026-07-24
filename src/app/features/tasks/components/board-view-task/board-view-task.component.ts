c7c14f91ed3022d71afc7a81a68eadd9eee314db
Christina <christina.i.aziz7@gmail.com>
2026-07-24 19:22:22 +0300

    feat: create initial structure for board view task component

---
+++ src/app/features/tasks/components/board-view-task/board-view-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-view-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './board-view-task.component.html',
+  styleUrl: './board-view-task.component.css'
+})
+export class BoardViewTaskComponent {
+
+}
