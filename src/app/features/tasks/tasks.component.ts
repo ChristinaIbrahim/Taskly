085dd70091031dd82281995dd8f300f33e7c88ae
Christina <christina.i.aziz7@gmail.com>
2026-07-25 13:33:11 +0300

    feat(tasks): integrate list-view-task and view switcher navigation logic

--- src/app/features/tasks/tasks.component.ts
+++ src/app/features/tasks/tasks.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { BoardViewTaskComponent } from './components/board-view-task/board-view-task.component';
-import { RouterLink } from '@angular/router';
+import { ListViewTaskComponent } from './components/list-view-task/list-view-task.component'; 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [BoardViewTaskComponent, CommonModule , RouterLink],
+  imports: [BoardViewTaskComponent, ListViewTaskComponent, CommonModule, RouterLink], 
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent implements OnInit {
   private route = inject(ActivatedRoute);
+  private router = inject(Router); 
   projectId = '';
   currentView = 'board';
 
@@ -28,6 +29,16 @@ export class TasksComponent implements OnInit {
 
     this.route.queryParamMap.subscribe((params) => {
       this.currentView = params.get('view') || 'board';
+    });
+  }
+  onViewChange(event: any): void {
+    const selectedView = event.target.value;
+    this.currentView = selectedView;
+
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { view: selectedView },
+      queryParamsHandling: 'merge'
     });
   }
 }
